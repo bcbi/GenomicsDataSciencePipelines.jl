52f2992a085b1107b09fed4a2e5fce4b3bddd6af
AshlinHarris <ashlin_harris@brown.edu>
2023-05-05 14:08:57 -0400

    Return to global scope

--- main.jl
+++ main.jl
@@ -18,47 +18,47 @@ function data_dictionary(x)
 	ddf[ddf[!,"Variable Name"] .== uppercase(x),:]
 end
 
-function main()
-	# Read in options
-	conf = ConfParse("./config.ini")
-	parse_conf!(conf)
-	data_dir = retrieve(conf, "local", "data_dir")
-	prior_file = retrieve(conf, "local", "prior_file")
-	variant_file = retrieve(conf, "local", "variant_file")
+# Read in options
+conf = ConfParse("./config.ini")
+parse_conf!(conf)
+data_dir = retrieve(conf, "local", "data_dir")
+prior_file = retrieve(conf, "local", "prior_file")
+variant_file = retrieve(conf, "local", "variant_file")
 
-	work_dir = retrieve(conf, "local", "work_dir")
-	dict_file = retrieve(conf, "local", "dict_file")
-	accession_file = retrieve(conf, "local", "accession_file")
+work_dir = retrieve(conf, "local", "work_dir")
+dict_file = retrieve(conf, "local", "dict_file")
+accession_file = retrieve(conf, "local", "accession_file")
 
-	# Read in data
-	vdf = CSV.File(joinpath(data_dir, variant_file),
-		       dateformat="mm/dd/yyyy",
-		       types=Dict(:idl_zipcode=>String, :test_site_zip=>String) ) |> DataFrame
-	ddf = CSV.read(joinpath(work_dir, dict_file), DataFrame, select=1:6)
-	gdf = CSV.read(joinpath(work_dir, accession_file), DataFrame)
+# Read in data
+vdf = CSV.File(joinpath(data_dir, variant_file),
+	       dateformat="mm/dd/yyyy",
+	       types=Dict(:idl_zipcode=>String, :test_site_zip=>String) ) |> DataFrame
+ddf = CSV.read(joinpath(work_dir, dict_file), DataFrame, select=1:6)
+gdf = CSV.read(joinpath(work_dir, accession_file), DataFrame)
 
-	# Remove unneeded columns
-	# Columns where all values are the same
-	bad_columns = names(vdf)[ vdf |> eachcol .|> allequal |> findall ]
-	# Delete these columns
-	select!(vdf, Not( bad_columns ))
+# Remove unneeded columns
+# Columns where all values are the same
+bad_columns = names(vdf)[ vdf |> eachcol .|> allequal |> findall ]
+# Delete these columns
+select!(vdf, Not( bad_columns ))
 
-	# Set proper datatype for true/false survey data
-	for col in names(vdf)
-		all_values = unique(vdf[!,col])
-		if all_values == [0,1]
-			vdf[!,col] = Bool.(vdf[!,col])
-		elseif all_values == ["No", "Yes"]
-			d = Dict(["No", "Yes"] .=> [false,true])
-			vdf[!,col] = vdf[!,col] .|> x->d[x]
-		end
+# Set proper datatype for true/false survey data
+for col in names(vdf)
+	all_values = unique(vdf[!,col])
+	if all_values == [0,1]
+		vdf[!,col] = Bool.(vdf[!,col])
+	elseif all_values == ["No", "Yes"]
+		d = Dict(["No", "Yes"] .=> [false,true])
+		vdf[!,col] = vdf[!,col] .|> x->d[x]
 	end
+end
 
-	# Fix names
-	rename!(vdf, names(vdf) .=> uppercase.(names(vdf)))
-	ddf[!,"Variable Name"] = uppercase.(ddf[!,"Variable Name"])
+# Fix names
+rename!(vdf, names(vdf) .=> uppercase.(names(vdf)))
+ddf[!,"Variable Name"] = uppercase.(ddf[!,"Variable Name"])
 
-	# Add ID to GISAID data matching the COVID-19 accession IDs
+# Add ID to GISAID data matching the COVID-19 accession IDs
+gdf.id = let
 	m = match.(r"^hCoV-19/USA/RI-RISHL-(.*)/20([0-9]{2})$",gdf.strain)
 	x = convert(Vector{Union{Nothing,String}}, fill(nothing,nrow(gdf)))
 	for i in 1:nrow(gdf)
@@ -66,5 +66,5 @@ function main()
 		isnothing(a) && continue
 		x[i] = "RI-" * a[2] * "-" * a[1]
 	end
-	gdf.id=x
+	x
 end
