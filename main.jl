fbc9ca066946a6346b81b6682e31c437a01e7359
AshlinHarris <ashlin_harris@brown.edu>
2023-08-10 14:15:31 -0400

    Format

--- main.jl
+++ main.jl
@@ -128,26 +128,27 @@ println()
 
 Plots.savefig(Plots.histogram(jdf.IDL_SPECIMEN_COLLECTION_DATE, bins=80), "collection_dates.png")
 
-
 function RI_plot(DATA, COLOR, FILENAME, TITLE)
-	# Geographical plot
+
 	cpop = CSV.File(joinpath(map_dir, "zips.csv"), types=Dict(:ZIP=>String)) |> DataFrame
 	cpop.PLOT_VALUE = cpop.ZIP .|> x -> get(DATA |> eachrow |> Dict, x, 0) |> Float64
 	# Downloaded from https://www.census.gov/geographies/mapping-files/time-series/geo/carto-boundary-file.html
 	counties = gmtread(joinpath(map_dir,"cb_2018_us_zcta510_500k/cb_2018_us_zcta510_500k.shp"))
 	dfc = DataFrame(ZIP = map(x->x.attrib["ZCTA5CE10"],counties),ORDER=1:length(counties))
+
 	joineddata = @chain leftjoin(dfc,cpop,on= [:ZIP],makeunique=true) begin
 		@orderby(:ORDER)
 	end
-	cptvallog = makecpt(range=(0,maximum(DATA[!,2])),C=COLOR)
-	# I think these are the options: 
-	# https://docs.juliaplots.org/latest/generated/colorschemes/#cmocean
-	# algae looks nice, but it doesn't show contrast well
-	# speed is okay
-	# I like deep
 	joineddata.PLOT_VALUE = replace(joineddata.PLOT_VALUE,missing => 0)
-	GMT.plot(counties,level=joineddata.PLOT_VALUE,cmap=cptvallog,close=true,fill="+z",pen=0.25,region=(-71.9,-71.1,41.1,42.05),
-		   proj=:guess,colorbar=true,figname=FILENAME,title=TITLE)
+
+	# I think these are the colorscheme options: 
+	# https://docs.juliaplots.org/latest/generated/colorschemes/#cmocean
+	# Some options that look nice are :algae, :deep, :matter, and :speed
+	GMT.plot(counties,level=joineddata.PLOT_VALUE,
+		cmap = makecpt(range=(0,maximum(DATA[!,2])),C=COLOR),
+		close=true, fill="+z",pen=0.25,region=(-71.9,-71.1,41.1,42.05),
+		proj=:guess,colorbar=true,figname=FILENAME,title=TITLE)
+
 end
 
 RI_plot(
